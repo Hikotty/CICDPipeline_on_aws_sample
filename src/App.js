d382371045d34e304708f0152920136e5497d7f8
Hikoto Iseda <105040278+Hikotty@users.noreply.github.com>
2025-03-05 13:15:34 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import './App.css';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>AWS CI/CD パイプラインデモ</h1>
+        <p>これはAWS CodePipeline、CodeBuild、CodeDeployを使用してデプロイされたReactアプリケーションです。</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
